630ed668f60d29ecb32b2f65e696b7b8597620be
kocurass@gmail.com <k.kapalka@hotmail.com>
2019-04-09 12:59:36 +0200

    TimeActivitysd

--- app/src/main/java/com/example/clock/MainActivity.java
+++ app/src/main/java/com/example/clock/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.clock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.time.*;
 
 public class MainActivity extends AppCompatActivity {
+    final static String KEY = "time_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void checkTime(View view) {
+            LocalTime time = LocalTime.now(ZoneId.of("Europe/Warsaw")).withNano(0);
+            Intent intent = new Intent(this,TimeActivity.class);
+            intent.putExtra(KEY, time.toString());
+            startActivity(intent);
     }
 }
--- app/src/main/java/com/example/clock/TimeActivity.java
+++ app/src/main/java/com/example/clock/TimeActivity.java
@@ -1,13 +1,53 @@
 package com.example.clock;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.TimeZone;
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class TimeActivity extends AppCompatActivity {
 
+    TextView war;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
+        war = findViewById(R.id.time0);
+        Intent intent = getIntent();
+        war.setText(intent.getStringExtra(MainActivity.KEY));
+
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask(){
+            @Override
+            public void run(){
+                nextTime();
+            }
+        };
+
+        timer.schedule(timerTask,1000,1000);
     }
+
+    private void nextTime(){
+
+        war.setText(String.valueOf(LocalTime.now().withNano(0)));
+
+        TextView lon = findViewById(R.id.time2);
+        lon.setText(String.valueOf(LocalTime.now(ZoneId.of("Europe/London")).withNano(0)));
+
+        TextView ny = findViewById(R.id.time1);
+        ny.setText(String.valueOf(LocalTime.now(ZoneId.of("America/Indianapolis")).withNano(0)));
+
+        TextView tk = findViewById(R.id.time4);
+        tk.setText(String.valueOf(LocalTime.now(ZoneId.of("Asia/Tokyo")).withNano(0)));
+
+    }
+
+
 }
